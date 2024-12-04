89b576ed39bea7c0794deab3982a90f0dada17b5
Nitin Khare <kharenitin777@gmail.com>
2024-12-04 17:46:02 +0530

    Added apis and react query hooks

---
+++ src/api/auth/index.js
@@ -0,0 +1,28 @@
+import axios from '@/config/axiosConfig';
+
+export const signUpRequest = async ({ email, password, username }) => {
+    try {
+        const response = axios.post("/users/signup", {
+            email,
+            password,
+            username
+        });
+        return response.data;
+    } catch (error) {
+        console.log(error);
+        throw error.response.data;
+    }
+}
+
+export const signInRequest = async ({ email, password }) => {
+    try {
+        const response = axios.post("/users/signin", {
+            email,
+            username
+        });
+        return response.data;
+    } catch (error) {
+        console.log(error);
+        throw error.response.data;
+    }
+}
