d341d389f4165195e3cd500ef1094be635e35510
Nitin Khare <kharenitin777@gmail.com>
2024-12-16 18:39:08 +0530

    Consumed useFetchWorkspace

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,6 +1,23 @@
 import { UserButton } from "@/components/atoms/UserButton/UserButton"
+import { useFetchWorkspace } from "@/hooks/apis/workspaces/useFetchWorkspace"
+import { useEffect } from "react"
 
 export const Home = () => {
+
+    const { isFetching, workspaces } = useFetchWorkspace()
+
+    useEffect(() => {
+
+        if(isFetching) return 
+
+        console.log("workspaces downloaded is", workspaces);
+        
+        if(workspaces.length === 0 || !workspaces) {
+            console.log('No workspaces found, creating one');
+        }
+
+    }, [ isFetching, workspaces ])
+
     return (
         <>
             Home
