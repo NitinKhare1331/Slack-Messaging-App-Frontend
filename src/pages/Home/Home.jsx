affba0b8842dbd649093e8f61c4b059243561e9d
Nitin Khare <kharenitin777@gmail.com>
2024-12-16 16:44:13 +0530

    Added user button

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,10 @@
+import { UserButton } from "@/components/atoms/UserButton/UserButton"
+
 export const Home = () => {
     return (
         <>
             Home
+            <UserButton />
         </>
     )
 }
