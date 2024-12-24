8acfa0dce4fe11fc41d201525ec9a5f550906f02
Nitin Khare <kharenitin777@gmail.com>
2024-12-24 14:56:14 +0530

    Added user item

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -8,6 +8,7 @@ import { Notfound } from '@/pages/Notfound/Notfound';
 import { ProtectedRoute } from '@/components/molecules/ProtectedRoute/ProtectedRoute';
 import { WorkspaceLayout } from '@/pages/workspace/Layout';
 import { JoinPage } from '@/pages/workspace/JoinPage';
+import { Channel } from './pages/workspace/Channel/Channel';
 
 
 export const AppRoutes = () => {
@@ -17,7 +18,7 @@ export const AppRoutes = () => {
             <Route path="/auth/signin" element={<Auth><SigninContainer /></Auth>} />
             <Route path='/home' element={<ProtectedRoute> <Home /> </ProtectedRoute>} />
             <Route path="/workspaces/:workspaceId" element={<ProtectedRoute><WorkspaceLayout>Workspace</WorkspaceLayout></ProtectedRoute>} />
-            <Route path="/workspaces/:workspaceId/channels/:channelId" element={<ProtectedRoute>Channel</ProtectedRoute>}/>
+            <Route path="/workspaces/:workspaceId/channels/:channelId" element={<ProtectedRoute><WorkspaceLayout><Channel /></WorkspaceLayout></ProtectedRoute>}/>
             <Route path="/workspaces/join/:workspaceId" element={<JoinPage />} />
             <Route path="/*" element={<Notfound />} />
         </Routes>
